8985e2e037ad2ba5e97d7f0ef980606693ce5937
zhuiqiuat <me@zqat.top>
2024-07-30 11:30:48 +0800

    防止末影龙破坏方块测试写法

---
+++ src/main/java/me/lele/worldSafe/listener/EnderDragonBlockDestructionProtectionListener.java
@@ -0,0 +1,40 @@
+package me.lele.worldSafe.listener;
+
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+
+public class EnderDragonBlockDestructionProtectionListener implements Listener {
+	private final List<String> worlds;
+
+	public EnderDragonBlockDestructionProtectionListener(List<String> worlds) {
+		this.worlds = worlds;
+	}
+
+	@EventHandler
+	void onEnderDragonDestroyBlock(EntityChangeBlockEvent e) {
+		// 判断是否为末影龙
+		if (e.getEntityType() != EntityType.ENDER_DRAGON)
+			return;
+		EnderDragon ed = (EnderDragon) e.getEntity();
+		// 判断方块和末影龙是否在同一世界
+		if (!e.getBlock().getWorld().equals(ed.getWorld()))
+			return;
+		// 判断世界是否为null值
+		if (ed.getWorld() == null) 
+			return;
+		// 判断此世界是否启用该功能
+		if (!worlds.contains(ed.getWorld().getName()))
+			return;
+		// 判断是否破坏方块
+		if (e.getTo() != Material.AIR)
+			return;
+		// 取消事件
+		e.setCancelled(true);
+	}
+}
