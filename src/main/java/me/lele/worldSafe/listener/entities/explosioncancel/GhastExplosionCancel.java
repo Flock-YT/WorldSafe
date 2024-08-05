584f4a8ff5d3b18573616255f3319a01fe57c265
Eric.乐乐 <mc3288732918@gmail.com>
2024-08-05 23:39:27 +0800

    fix - ✅ **ghastExplosionCancel** - 禁止 恶魂火球 爆炸

--- src/main/java/me/lele/worldSafe/listener/entities/explosioncancel/GhastExplosionCancel.java
+++ src/main/java/me/lele/worldSafe/listener/entities/explosioncancel/GhastExplosionCancel.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Ghast;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 import java.util.List;
@@ -29,6 +30,26 @@ public class GhastExplosionCancel implements Listener {
 		if (!(ent.getShooter() instanceof Ghast))
 			return;
 		// 清空受影响的方块
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+		// 检测造成伤害的实体是否为火球
+		System.out.println(1);
+		if (e.getDamager().getType() != EntityType.FIREBALL)
+			return;
+		Fireball ent = (Fireball) e.getDamager();
+		// 检测此世界是否启用
+		System.out.println(2);
+		if (!worlds.contains(ent.getWorld().getName()))
+			return;
+		// 检测是否为恶魂发出的火球
+		System.out.println(3);
+		if (!(ent.getShooter() instanceof Ghast))
+			return;
+		//消除火球伤害
+		System.out.println(4);
 		e.setCancelled(true);
 	}
 }
