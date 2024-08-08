db2064e8e3beae65f405f2445d2f3d7c87b74209
zhuiqiuat <me@zqat.top>
2024-08-08 19:11:29 +0800

    更新禁止床爆炸

--- src/main/java/me/lele/worldSafe/listener/blocks/explosioncancel/BedExplosionCancelListener.java
+++ src/main/java/me/lele/worldSafe/listener/blocks/explosioncancel/BedExplosionCancelListener.java
@@ -1,5 +1,7 @@
 package me.lele.worldSafe.listener.blocks.explosioncancel;
 
+import org.bukkit.World;
+import org.bukkit.World.Environment;
 import org.bukkit.block.data.type.Bed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,30 +12,33 @@ import java.util.List;
 
 public class BedExplosionCancelListener implements Listener {
 
-    private final List<String> worlds;
+	private final List<String> worlds;
 
-    public BedExplosionCancelListener(List<String> worlds) {
-        this.worlds = worlds;
-    }
+	public BedExplosionCancelListener(List<String> worlds) {
+		this.worlds = worlds;
+	}
 
-    @EventHandler
-    void onPlayerInteractEvent(PlayerInteractEvent e) {
-        // 检查玩家是否为右键点击
-        if (e.getAction() != Action.RIGHT_CLICK_BLOCK)
-            return;
-        //检查玩家点击的是否为方块
-        if (!e.hasBlock())
-            return;
-        //检查玩家点击的是否为床
-        if (e.getClickedBlock().getBlockData() instanceof Bed){
-            // 判断是否启用这个世界
-            if (!worlds.contains(e.getPlayer().getWorld().getName()))
-                return;
-            // 取消事件，防止爆炸
-            e.setCancelled(true);
+	@EventHandler
+	void onPlayerInteractEvent(PlayerInteractEvent e) {
+		// 检查玩家是否为右键点击
+		if (e.getAction() != Action.RIGHT_CLICK_BLOCK)
+			return;
+		// 检查玩家点击的是否为方块
+		if (!e.hasBlock())
+			return;
+		World w = e.getClickedBlock().getWorld();
+		// 判断是否在主世界触发
+		if (w.getEnvironment() == Environment.NORMAL)
+			return;
+		// 检查玩家点击的是否为床
+		if (!(e.getClickedBlock().getBlockData() instanceof Bed))
+			return;
+		// 判断是否启用这个世界
+		if (!worlds.contains(w.getName()))
+			return;
+		// 取消事件，防止爆炸
+		e.setCancelled(true);
 
-        }
-
-    }
+	}
 
 }
